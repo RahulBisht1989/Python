76a631880d4f0310ba6b8699ab9e1322a2a8ff03
RahulBisht1989 <rahulbisht1989@gmail.com>
2026-05-15 17:35:32 +0530

    more files

--- hogwarts.py
+++ hogwarts.py
@@ -1,7 +1,23 @@
-students = ["Rahul","Bobby","Shetal","Anaysha","Shivu"]
+#students = ["Rahul","Bobby","Shetal","Anaysha","Shivu"]
 
-# for student in students:
-#     print(student)
+# # for student in students:
+# #     print(student)
 
-for i in range(len(students)):
-    print(students[i])
+# for i in range(len(students)):
+#     print(students[i])
+
+students = [
+    {
+        "name":"Rahul",
+        "house":"Panther",
+        "id":7687
+    },
+    {
+        "name":"Bobby",
+        "house":"hippopotamus",
+        "id": 787
+    }
+]
+
+for student in students:
+    print(f"my name is {student['name']} , my house is {student['house']} and badge number is {student['id']}")
